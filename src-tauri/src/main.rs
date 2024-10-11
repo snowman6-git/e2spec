46d6f214772d7f3aa48138f2319b2ec817da3688
snowman6-git <chldlfgks55@gmail.com>
2024-10-11 15:21:51 +0900

    modified:   .gitignore 	modified:   src-tauri/src/main.rs 	modified:   src/main.js

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,9 +4,10 @@ fn main() {
     // tauri_app_lib::run();
     tauri::Builder::default()
     // .invoke_handler(tauri::generate_handler![link])
-    .invoke_handler(tauri::generate_handler![sysinfo])
+    .invoke_handler(tauri::generate_handler![sysinfo, ramfresh])
     .run(tauri::generate_context!())
     .expect("failed to run app");
+
 }
 
 // use reqwest::Client;
@@ -14,9 +15,22 @@ use serde_json::{json};
 use sysinfo::{
     System,
 };
-fn togb(value: u64) -> f64 {(value as f64) / 1024_f64.powf(3.0)}
+fn togb(value: u64) -> String {
+    format!("{:.2}GB", (value as f64) / 1024_f64.powf(3.0))
+}
 
 
+#[tauri::command]
+fn ramfresh() -> serde_json::Value{ //리턴값을 적어야함, 패키지 밸류는 이런식
+    let mut sys = System::new_all();
+    sys.refresh_all();
+    let info = json!({
+        "mem_used": togb(sys.used_memory()),
+        "mem_total": togb(sys.total_memory()),
+        "vmem_total": togb(sys.total_swap()),
+    });
+    info.into()
+}
 
 #[tauri::command]
 fn sysinfo() -> serde_json::Value{ //리턴값을 적어야함, 패키지 밸류는 이런식
--- src/main.js
+++ src/main.js
@@ -8,23 +8,33 @@ async function greet() {
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
 }
 
-function cl(text){
-  console.log(text)
+function cl(text){console.log(text)}
+function edit(target, text){document.querySelector(target).textContent = text}
+function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms))}
+
+
+async function ramfresh() {
+  const intervalId = setInterval(() => {
+    invoke('ramfresh').then(
+      (info) => {
+        edit("#ram", `${info["mem_used"]}/${info["mem_total"]} +(v${info["vmem_total"]})`)
+        cl(info["mem_used"]) 
+      }
+    )
+  }, 1000);
 }
-function edit(target, text){
-  document.querySelector(target).textContent = text
-}
-window.addEventListener("DOMContentLoaded", () => {
-  
+
+
+window.addEventListener("DOMContentLoaded", () => {  
   invoke('sysinfo').then(
     (info) => {
       edit("#OS", info["OS"])
       edit("#cpu", info["CPU_len"])
-      edit("#ram", info["mem_used"])
-      edit("#result", "테스트")
+      edit("#result", "")
     }
   )
-  
+  ramfresh()
+  //ram refresh
   
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
